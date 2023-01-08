75f57d4bbeb154a42df18b947a5632775752bbc9
Dmitriy Shten <dmitriy.shten@payquity.io>
2023-01-08 15:28:59 +0300

    #12 - test...

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './store';
 
 import App from './App';
@@ -12,8 +12,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 );
