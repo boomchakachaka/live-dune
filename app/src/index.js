244d24cd3b219b92074e652ae0c2a7fde68751d2
Dmitriy Shten <dmitriy.shten@payquity.io>
2023-01-08 20:37:45 +0300

    test!

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,9 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { Provider } from 'react-redux';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-// import { store } from './store';
+import { store } from './store';
 
 import App from './App';
 
@@ -11,9 +11,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
-  // <Provider store={store}>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  // </Provider>
+  </Provider>
 );
