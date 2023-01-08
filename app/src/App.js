600cbb89deedd20112ba9d30562d4d05642a711c
Dmitriy Shten <dmitriy.shten@payquity.io>
2023-01-08 18:48:53 +0300

    #12 - test...

--- app/src/App.js
+++ app/src/App.js
@@ -17,7 +17,7 @@ function App() {
       <header className={styles['header']}><Header /></header>
       <div className={styles['content']}>
         <Routes>
-          <Route path={process.env['PUBLIC_URL']} element={<Navigate replace to={process.env['PUBLIC_URL'] + '/sign-in'} />} />
+          <Route path={'/'} element={<Navigate replace to={process.env['PUBLIC_URL'] + '/sign-in'} />} />
           <Route path={process.env['PUBLIC_URL'] + '/sign-in'} element={<SignInPage />} />
           <Route path={process.env['PUBLIC_URL'] + '/sign-up'} element={<SignUpPage />}/>
           <Route path={process.env['PUBLIC_URL'] + '/reset-password'} element={<ResetPasswordPage />} />
