f92256398d5aa8906aa10bdc3bbae2e000d5e208
Dmitriy Shten <dmitriy.shten@payquity.io>
2023-01-08 19:46:13 +0300

    #12 - test...

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,15 +1,15 @@
 
-import { name as signUpReducerName, reducer as signUpReducer } from '@modules/sign-up';
-import { name as resetPasswordReducerName, reducer as resetPasswordReducer } from '@modules/reset-password';
-import { reducer as formReducer } from 'redux-form';
+// import { name as signUpReducerName, reducer as signUpReducer } from '@modules/sign-up';
+// import { name as resetPasswordReducerName, reducer as resetPasswordReducer } from '@modules/reset-password';
+// import { reducer as formReducer } from 'redux-form';
 
 import { configureStore } from '@reduxjs/toolkit';
 
 
-export const store = configureStore({
-  reducer: {
-    form: formReducer,
-    [signUpReducerName]: signUpReducer,
-    [resetPasswordReducerName]: resetPasswordReducer,
-  },
-});
+// export const store = configureStore({
+//   reducer: {
+//   //   form: formReducer,
+//   //   [signUpReducerName]: signUpReducer,
+//   //   [resetPasswordReducerName]: resetPasswordReducer,
+//   // },
+// });
